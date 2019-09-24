bf3215ac0b45927cde6620aa8604be88591085cc
higasi <higasi.nba1209@au.com>
2019-09-24 17:30:31 +0900

    hh

--- db/migrate/20190924075309_create_posts.rb
+++ db/migrate/20190924075309_create_posts.rb
@@ -1,8 +1,17 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      
-      t.timestamps
+      t.string :name
+      t.string :image
+      t.string :introduction
+      t.string :regist            #チャンネル登録者数
+      t.string :characomment1
+      t.string :yutube            #動画
+      t.string :mycomment
+      t.string :celebcomment      #有名人コメント
+      t.string :symmetry          #相性
+      t.string :category
+      t.timestamps null: true
     end
   end
 end
