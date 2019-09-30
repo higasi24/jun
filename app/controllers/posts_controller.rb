667ece593d3eca78a86902b02c634fa9c32836fd
higasi <higasi.nba1209@au.com>
2019-09-30 18:49:43 +0900

    l

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @yyyymm = params[:yyyymm]
     @contents = @post.contents.group_by {|content| content.created_at.strftime('%Y%m')[@yyyymm]}
-    @p = @contents.values
+    if @contents.keys[0] == nil
+      @tt = @contents.to_a[1]
+      @p = @tt[1]
+      # @p = @e[0]
+      # binding.pry
+    else @contents.keys[1] == nil
+      @tt = @contents.to_a[0]
+      @p = @tt[1]
+    end
+    # binding.pry
+    # if @contents.keys == nil
+    #   nil.delete
+    #   @p = @contents.values
+    # else 
+    #   @p = @contents.values
+    # end
+    # @p = @contents.values
     # @ww = post[0]
     # .sort_by{|content| content[:created_at]}.reverse
     # .group_by {|content| content.created_at.strftime('%Y%m')[@yyyymm]}
